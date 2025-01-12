a39a3816878b5344a51f9d5a1599c46d3c1c2bb0
krlpopiel <45494004+krlpopiel@users.noreply.github.com>
2025-01-12 15:14:29 +0100

    Update model_vgg16.py

--- zadanie_projektowe/model_vgg16.py
+++ zadanie_projektowe/model_vgg16.py
@@ -1,7 +1,8 @@
 import torch.nn as nn
 from torchvision.models import vgg16, VGG16_Weights
+import torch
 
-def get_vgg16(num_classes=39, pretrained=True):
+def get_vgg16(num_classes=39, pretrained=True, weights_path=None):
     if pretrained:
         weights = VGG16_Weights.DEFAULT
         model = vgg16(weights=weights)
@@ -9,4 +10,9 @@ def get_vgg16(num_classes=39, pretrained=True):
         model = vgg16(weights=None)
 
     model.classifier[6] = nn.Linear(model.classifier[6].in_features, num_classes)
+
+    # Wczytaj wagi z pliku, jeśli podano ścieżkę
+    if weights_path:
+        model.load_state_dict(torch.load(weights_path))
+    
     return model
